cbf297d194bd47706b3f38fc3cd7073311d2e0c7
johanbrook <jakob_honor@rocketmail.com>
2011-03-27 21:35:24 +0200

    Added global Javascript THEME_DIR variable.

--- footer.php
+++ footer.php
@@ -13,6 +13,10 @@
 	
 		
 <?php wp_footer(); ?>
+<script type="text/javascript">
+	// Sets a window variable to the theme's absolute path
+	window.THEME_DIR = "<?php echo JB_THEME_DIR;?>";
+</script>
 <script src="<?php echo JB_JS_DIR;?>jquery.smoothscroll.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php echo JB_JS_DIR;?>whiteboard.js" type="text/javascript" charset="utf-8"></script>
 
