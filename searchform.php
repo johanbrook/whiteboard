4317f2332c6ed52d1f3211924ce25c0aeba45122
Johan Brook <johan.jvb@gmail.com>
2011-06-22 23:00:27 +0200

    Cleaned up searchform.

--- searchform.php
+++ searchform.php
@@ -1,9 +1,6 @@
-<?php global $is_gecko, $is_IE;?>
-
-<form role="search" action="<?php bloginfo("home");?>" method="GET" id="searchform">
-	<input type="search" name="s" id="s" placeholder="Search and you will find" title="type keywords and press <enter>" size="30"
+<form role="search" action="<?php echo get_option("home");?>" method="GET">
+	<input type="search" name="s" placeholder="Search and you will find" size="30"
 		results="0"
 		<?php if(is_search()):?> value="<?php the_search_query();?>" <?php endif;?>
-		<?php if($is_gecko || $is_IE):?> value="Search and you will find" onclick="this.select();"<?php endif; ?>
 	 />
 </form>
